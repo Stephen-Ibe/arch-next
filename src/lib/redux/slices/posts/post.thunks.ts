0fdec9718882012a161ed8e6cb1a6f4657d57e4c
Stephen Ibe <saintkalson@gmail.com>
2024-09-12 08:22:18 +0100

    export middlewares

--- src/lib/redux/slices/posts/post.thunks.ts
+++ src/lib/redux/slices/posts/post.thunks.ts
@@ -1,19 +1,11 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-type TSerializedError = {
-  name?: string;
-  message?: string;
-  stack?: string;
-  code?: string;
-};
-
 export const fetchPosts = createAsyncThunk(
   'posts',
   async (_, { rejectWithValue }) => {
     try {
       const res = await axios.get('https://jsonplaceholder.typicode.com/poss/');
-      console.error(res);
       return res.data;
     } catch (error: any) {
       return rejectWithValue(error);
